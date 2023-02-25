98bc12d36bb7378bd135e10dedadba616d49abad
Nikita Nafranets <root@dimensi.dev>
2023-02-25 04:02:27 +0300

    Initial Commit

---
+++ Anime365/Anime365App.swift
@@ -0,0 +1,17 @@
+//
+//  Anime365App.swift
+//  Anime365
+//
+//  Created by Nikita Nafranets on 25.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Anime365App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
